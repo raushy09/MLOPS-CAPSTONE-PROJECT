cdaecb59f8caf374c83c130cb017904cf83691e0
Raushan <1109raushan@gmail.com>
2026-06-18 08:56:28 +0530

    project structure ready

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this is aan mlops project',
+    author='Raushan Kumar',
+    license='MIT',
+)
