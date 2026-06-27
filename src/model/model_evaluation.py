6c730d5d75ce1bff3a34e2e65020eb1cc59d7f5a
Raushan <1109raushan@gmail.com>
2026-06-27 23:08:40 +0530

    cicd run-7

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -18,7 +18,7 @@ dagshub_token = os.getenv("CAPSTONE_TEST")
 if not dagshub_token:
     raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
 
-os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_USERNAME"] = "raushankumar733329"
 os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
 dagshub_url = "https://dagshub.com"
